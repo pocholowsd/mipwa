f779e81523b09936978b73d69983a60540e31d48
franciscojgonzalezs <francisco.gonzalez@chapala.tecmm.edu.mx>
2021-09-23 09:09:31 -0500

    PRIMER SUBIDA

---
+++ sw.js
@@ -0,0 +1,20 @@
+//service worker -> SW
+const staticDev = "dev-mipwa-v1";
+const assets = [
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
